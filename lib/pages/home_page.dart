f329de90ff14f4dd53b2e6e454ea370cc4a6856f
faiq <faiq@voltras.co.id>
2025-07-24 17:11:28 +0700

    tes image 17

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget {
       width: size.width,
       child: Center(
         child: ColoredBox(
-          color: Colors.blue,
+          color: Colors.red,
           child: Image.asset('assets/groom.png', height: 300, width: 200, fit: BoxFit.cover),
         ),
       ),
