037f1c291acda1e45b9d771407ac98d87dc7c568
faiq <faiq@voltras.co.id>
2025-07-24 15:25:19 +0700

    tes image 9

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,22 +6,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return const SizedBox.shrink();
     final size = MediaQuery.of(context).size;
     return SizedBox(
       height: size.height,
       width: size.width,
       child: Center(
         child: ColoredBox(
-          color: Colors.red,
-          child: Image.asset(
-            // 'https://iv-project-web-app.vercel.app/assets/packages/iv_project_invitation_theme/assets/dummys/try_theme_cover_image.jpg',
-            'assets/dummys/try_theme_cover_image.jpg',
-            // package: 'iv_project_invitation_theme',
-            height: 300,
-            width: 200,
-            fit: BoxFit.cover,
-          ),
+          color: Colors.amber,
+          child: Image.asset('assets/dummys/try_theme_cover_image.jpg', height: 300, width: 200, fit: BoxFit.cover),
         ),
       ),
     );
