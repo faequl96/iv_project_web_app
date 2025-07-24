c36b09451edbd75e2944d646a761c98e3d0eab1c
faiq <faiq@voltras.co.id>
2025-07-24 15:59:04 +0700

    tes image 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 // import 'package:iv_project_core/iv_project_core.dart';
 import 'package:iv_project_web_app/app.dart';
-// import 'package:iv_project_web_app/routes/router.dart';
+import 'package:iv_project_web_app/core/navigation_service.dart';
+import 'package:iv_project_web_app/routes/router.dart';
 
 void main() {
-  // NavigationService.init(router);
+  NavigationService.init(router);
 
   print('tes4');
   runApp(const App());
