295459d8229548c74ddeeeef741e88c7f736734b
faiq <faiq@voltras.co.id>
2025-07-24 15:07:26 +0700

    tes image 8

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,6 +7,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return const SizedBox.shrink();
-    return InvitationThemeLauncher(invitation: Invitation.create(themeId: 1, themeName: 'Try Theme'));
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height,
+      width: size.width,
+      child: Center(
+        child: ColoredBox(
+          color: Colors.red,
+          child: Image.asset(
+            // 'https://iv-project-web-app.vercel.app/assets/packages/iv_project_invitation_theme/assets/dummys/try_theme_cover_image.jpg',
+            'assets/dummys/try_theme_cover_image.jpg',
+            // package: 'iv_project_invitation_theme',
+            height: 300,
+            width: 200,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+    // return InvitationThemeLauncher(invitation: Invitation.create(themeId: 1, themeName: 'Try Theme'));
   }
 }
