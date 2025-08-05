8989a1c38ea219e8e42ec8da691f3d679aada418
faiq <faiq@voltras.co.id>
2025-08-06 00:01:46 +0700

    improve

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:iv_project_core/iv_project_core.dart';
 import 'package:iv_project_web_app/core/di/app_bloc_providers.dart';
 
@@ -23,17 +22,6 @@ class _App extends StatefulWidget {
 class _AppState extends State<_App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: NavigationService.router,
-      theme: ThemeData(
-        textTheme: GoogleFonts.ibmPlexSansTextTheme(),
-        inputDecorationTheme: const InputDecorationTheme(floatingLabelStyle: TextStyle(color: AppColor.primaryColor)),
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: AppColor.primaryColor,
-          selectionHandleColor: AppColor.primaryColor,
-        ),
-      ),
-    );
+    return MaterialApp.router(debugShowCheckedModeBanner: false, routerConfig: NavigationService.router);
   }
 }
