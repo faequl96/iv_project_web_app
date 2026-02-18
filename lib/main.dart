db5248cc33f46b47e0061955ae07fa4aec2b891c
faiq <faiq@voltras.co.id>
2026-02-19 05:39:09 +0700

    change apiUrl

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 void main() async {
   usePathUrlStrategy();
 
-  ApiUrl.set('https://02eb-175-103-42-210.ngrok-free.app');
+  ApiUrl.set('https://e5d3-110-137-193-158.ngrok-free.app');
 
   NavigationService.init(router);
 
