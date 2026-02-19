66a4347fe68022f9abacdea9e12994b01c02cc48
faiq <faiq@voltras.co.id>
2026-02-19 12:17:59 +0700

    test deploy 2

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:flutter_web_plugins/url_strategy.dart';
 void main() async {
   usePathUrlStrategy();
 
-  print('test deploy');
+  print('test deploy 2');
 
   ApiUrl.set('https://e5d3-110-137-193-158.ngrok-free.app');
 
