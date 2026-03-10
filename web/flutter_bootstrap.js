02de6cd0a5d646da92ae52f58a3c470a1faadc2b
faiq <90074652+faequl96@users.noreply.github.com>
2026-03-10 13:39:19 +0700

    test splash 7

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -6,28 +6,43 @@ const progressText = document.getElementById('progress_text');
 const statusText = document.getElementById('status_text');
 const loaderWrapper = document.getElementById('loading_indicator');
 
+let currentPercent = 0;
+let progressInterval;
+
+function updateFakeProgress(targetPercent) {
+  clearInterval(progressInterval);
+
+  progressInterval = setInterval(() => {
+    if (currentPercent < targetPercent) {
+      currentPercent += 1;
+      
+      if (progressBar) progressBar.style.width = currentPercent + '%';
+      if (progressText) progressText.textContent = currentPercent + '%';
+
+      if (statusText) {
+        if (currentPercent < 40) statusText.textContent = "Downloading engine...";
+        else if (currentPercent < 80) statusText.textContent = "Preparing assets...";
+        else if (currentPercent < 100) statusText.textContent = "Finalizing...";
+        else statusText.textContent = "Launching App...";
+      }
+    } else {
+      clearInterval(progressInterval);
+      if (currentPercent >= 100 && loaderWrapper) {
+        setTimeout(() => {
+          loaderWrapper.style.opacity = '0';
+          setTimeout(() => loaderWrapper.remove(), 400);
+        }, 200);
+      }
+    }
+  }, 10);
+}
+
 _flutter.loader.load({
-  onProgress: function(value) {
-    console.log("Loading progress:", value);
-
-    const percent = Math.round(value * 100);
-    if (progressBar) progressBar.style.width = percent + '%';
-    if (progressText) progressText.textContent = percent + '%';
-
-    if (statusText) {
-      if (percent < 40) statusText.textContent = "Downloading engine...";
-      else if (percent < 80) statusText.textContent = "Preparing assets...";
-      else statusText.textContent = "Finalizing...";
-    }
-  },
   onEntrypointLoaded: async function(engineInitializer) {
+    updateFakeProgress(75);
     const appRunner = await engineInitializer.initializeEngine();
-
-    if (loaderWrapper) {
-      loaderWrapper.style.opacity = '0';
-      setTimeout(() => loaderWrapper.remove(), 400);
-    }
-
+    updateFakeProgress(85);
     await appRunner.runApp();
+    updateFakeProgress(100);
   }
 });
